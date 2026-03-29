c8cbd53b2efd792541731a48ffd846997ad03e4d
Lucasechavarria <lucasechavarria1986@gmail.com>
2026-03-29 14:05:25 -0300

    Fix: Usar get_user_model en create_admin.py para modelo Usuario personalizado

--- create_admin.py
+++ create_admin.py
@@ -4,11 +4,20 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def create_user(username, email, password):
     if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
+        User.objects.create_superuser(
+            username=username, 
+            email=email, 
+            password=password, 
+            nombre='Admin', 
+            apellido='Sistema', 
+            celular=f"00000{username[:10]}" # Celular ficticio unico
+        )
         print(f"Superusuario {username} creado con éxito.")
     else:
         print(f"El superusuario {username} ya existe.")
